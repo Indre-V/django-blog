f07d6caadaaf5c559c147544e65f9d1f96cdd048
Indre-V <vilickaite@yahoo.ie>
2024-04-25 17:59:11 +0100

    updates

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,6 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1) 
+    # This means that we can now leave some posts in Draft while we finish them, and they will not show up on the live blog.
     template_name = "post_list.html"
