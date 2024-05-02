2ab3ac8ca86370671aaf1db750750a1d3791afbc
Indre-V <vilickaite@yahoo.ie>
2024-05-02 09:44:51 +0100

    set up comment count

--- blog/models.py
+++ blog/models.py
@@ -26,14 +26,15 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
-    name = models.CharField(max_length=80, blank=True)
-    email = models.EmailField(default=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.author}"
--- codestar/urls.py
+++ codestar/urls.py
@@ -19,10 +19,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("about/", include("about.urls"), name="about-urls"),
     path("accounts/", include("allauth.urls")),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
     path('editor/', include('django_summernote.urls')),
-    path('admin/', admin.site.urls),
 ]
